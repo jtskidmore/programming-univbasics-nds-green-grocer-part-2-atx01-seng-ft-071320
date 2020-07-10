17c40e95f5200d4726f9a463eb8532b5c1f7c353
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-10 21:46:59 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -7,13 +7,20 @@ def apply_coupons(cart, coupons)
   
   coupons.each do |coupon|
     item = find_item_by_name_in_collection(coupon[:item], cart)
-    discounted_item_cost = coupon[:cost]/coupon[:num]
+    
+    
+    couponed_item_name = item[:name] + "W/COUPON"
+    couponed_item_cost = coupon[:cost]/coupon[:num]
+    couponed_item_clearance = item[:clearance]
+    couponed_item_count = coupon[:num]
+    
+    
     if item != nil
       if item[:count] >= coupon[:num]
-        
+        item[:count] -= coupon[:num]
+        cart << {:item => couponed_item_name, :price => couponed_item_cost, :clearance => couponed_item_clearance, :count => couponed_item_count}
       end
-      counter += 1
     end
   end
-  
+  puts cart
 end
